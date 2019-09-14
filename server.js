f70f6f691652a3fe6a52b31408c07024319e9b64
ENRIQUE RIVERA <KIK.RDL3@GMAIL.COM>
2019-09-13 17:06:44 -0700

    ready to trial deploy

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+//routes
+app.get('/', (request, response) => {
+  response.redirect('index.html');
+})
+
+app.get('/hello', (request, response) => {
+  response.send('Hello');
+})
+
+app.get('/data', (request, response) => {
+  let chris = {
+    name: 'Chris',
+    hair: 'dark brown',
+    eyes: 'dark',
+    favoriteFood: 'ass',
+    favoriteDrink: 'beer'
+  }
+
+  response.status(200).json(chris);
+})
+
+app.use('*', (request, response) =>{
+  response.status(404).send('Sorry, that route does not existe');
+})
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
